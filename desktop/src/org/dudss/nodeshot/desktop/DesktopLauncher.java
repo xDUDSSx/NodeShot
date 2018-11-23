0b067603e148d07cb14a342eb1b7ce39e84ebb26
Dan <rakusan.dan@gmail.com>
2018-11-23 20:08:53 +0100

    ErrorManager exception reporting fully functional.

--- desktop/src/org/dudss/nodeshot/desktop/DesktopLauncher.java
+++ desktop/src/org/dudss/nodeshot/desktop/DesktopLauncher.java
@@ -2,10 +2,10 @@ package org.dudss.nodeshot.desktop;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 
-import javax.swing.SwingUtilities;
-
 import org.dudss.nodeshot.Base;
 import org.dudss.nodeshot.BaseClass;
+import org.dudss.nodeshot.error.ErrorManager;
+import org.dudss.nodeshot.error.ErrorReporter;
 import org.dudss.nodeshot.screens.MenuScreen;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
@@ -16,9 +16,6 @@ public class DesktopLauncher {
 	
 	/**NodeEngine development version*/
 	public static final String ver = "v4.0";
-	
-	/**Global error manager used to invoke individual {@link ErrorReporter} objects*/
-	public static ErrorManager errorManager;
 	
 	/**The main method*/
 	public static void main (String[] arg) {
@@ -32,8 +29,6 @@ public class DesktopLauncher {
 		config.title = "NodeEngine." + ver + " OpenGL";
 		config.fullscreen = Base.fullscreen;
 		
-		errorManager = new ErrorManager();
-		
 		//Dialogs.showErrorDialog(GameScreen.stage, "A runtime error occured.", ex);
 		
 		//Uncaught error handling
@@ -41,7 +36,7 @@ public class DesktopLauncher {
 			@Override
 			public void uncaughtException (Thread thread, final Throwable ex) {	  	        					
 				//Call the global error manager and invoke a reporter to display the exception.
-				errorManager.report(ex, "An unexpected runtime error occurred!");				
+				org.dudss.nodeshot.BaseClass.errorManager.report(ex, "An unexpected runtime error occurred!");				
 			}
 		});
 		
